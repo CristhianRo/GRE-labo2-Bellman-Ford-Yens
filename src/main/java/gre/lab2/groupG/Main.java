635dffe014762d2f37af6e49825140d8db7caa97
CristhianRo <114008453+CristhianRo@users.noreply.github.com>
2024-05-24 11:13:26 +0200

    circuit négatif

--- src/main/java/gre/lab2/groupG/Main.java
+++ src/main/java/gre/lab2/groupG/Main.java
@@ -21,10 +21,26 @@ public final class Main {
     // BFYResult result = bfy.compute(graph, 0);
     // System.out.println(result);
 
+    WeightedDigraph graph51 = WeightedDigraphReader.fromFile("data/reseau5_1.txt");
+    BellmanFordYensAlgorithm bfy1 = new BellmanFordYensAlgorithm();
+    BFYResult result51 = bfy1.compute(graph51, 0);
+    System.out.println(result51);
+    //(0,—) (1,3) (5,1) (4,0)
+
+    //***********************************************************************
     WeightedDigraph graph52 = WeightedDigraphReader.fromFile("data/reseau5_2.txt");
-    BellmanFordYensAlgorithm bfy = new BellmanFordYensAlgorithm();
-    BFYResult result52 = bfy.compute(graph52, 0);
+    BellmanFordYensAlgorithm bfy2 = new BellmanFordYensAlgorithm();
+    BFYResult result52 = bfy2.compute(graph52, 0);
     System.out.println(result52);
+    //(0,—) (0,3) (4,1) (1,0) (3,2) (∞,—)
+
+    //***********************************************************************
+    WeightedDigraph graph53 = WeightedDigraphReader.fromFile("data/reseau5_3.txt");
+    BellmanFordYensAlgorithm bfy3 = new BellmanFordYensAlgorithm();
+    BFYResult result53 = bfy3.compute(graph53, 0);
+    System.out.println(result53);
+    //  Le réseau contient donc un circuit à coût négatif accessible depuis le sommet 1.
+
 
   }
 }
