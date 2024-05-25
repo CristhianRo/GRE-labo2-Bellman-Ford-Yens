5c61af8f4e05fde2914644b6adf825ecb23d592e
CristhianRo <114008453+CristhianRo@users.noreply.github.com>
2024-05-25 21:03:07 +0200

    version finale

--- src/main/java/gre/lab2/groupG/BellmanFordYensAlgorithm.java
+++ src/main/java/gre/lab2/groupG/BellmanFordYensAlgorithm.java
@@ -6,36 +6,45 @@ import gre.lab2.graph.BFYResult;
 import gre.lab2.graph.IBellmanFordYensAlgorithm;
 import gre.lab2.graph.WeightedDigraph;
 
+/**
+ * This class implements the Bellman-Ford-Yens algorithm for computing the shortest path tree or
+ * detecting a negative cycle in a weighted directed graph.
+ * <p>
+ * The Bellman-Ford-Yens algorithm is a variant of the Bellman-Ford algorithm that can detect negative cycles in a graph.
+ * It works by running the Bellman-Ford algorithm multiple times, each time with a different vertex as the source.
+ * If a negative cycle is reachable from the source vertex, the algorithm will detect it.
+ *
+ * @author Cristhian Ronquillo
+ */
 public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm {
 
   private static final int SENTINEL = -2;
 
   /**
  * This method computes the shortest path tree or detects a negative cycle in a weighted directed graph using the Bellman-Ford algorithm.
- *
  * @param graph The weighted directed graph in which to compute the shortest path tree or detect a negative cycle.
  * @param from The source vertex from which to compute the shortest path tree.
- * @return A BFYResult object that represents either the shortest path tree or a negative cycle.
+ * @return A {@link BFYResult} object that represents either the shortest path tree or a negative cycle.
  */
 @Override
 public BFYResult compute(WeightedDigraph graph, int from) {
   // The number of vertices in the graph.
   int nbVertices = graph.getNVertices();
   // The array of shortest path distances from the source to each vertex.
-  int[] distance = new int[nbVertices];
+  int[] distances = new int[nbVertices];
   // The array of predecessors for each vertex on the shortest path from the source.
-  int[] predecessor = new int[nbVertices];
+  int[] predecessors = new int[nbVertices];
   // The queue of vertices to be processed.
   Queue<Integer> queue = new ArrayDeque<>();
   // The array that keeps track of whether a vertex is in the queue.
   boolean[] inQueue = new boolean[nbVertices];
 
   // Initialize the distances and predecessors.
-  Arrays.fill(distance, Integer.MAX_VALUE);
-  Arrays.fill(predecessor, BFYResult.UNREACHABLE);
+  Arrays.fill(distances, Integer.MAX_VALUE);
+  Arrays.fill(predecessors, BFYResult.UNREACHABLE);
 
-  // The distance from the source to itself is 0.
-  distance[from] = 0;
+  // The distances from the source to itself is 0.
+  distances[from] = 0;
   // The counter for the number of iterations.
   int counterIterations = 0;
 
@@ -60,7 +69,7 @@ public BFYResult compute(WeightedDigraph graph, int from) {
         // If the number of iterations equals the number of vertices...
         if (counterIterations == nbVertices) {
           // ...then a negative cycle has been detected because there are still possible improvements.
-          List<Integer> cycle = findNegativeCycle(predecessor);
+          List<Integer> cycle = findNegativeCycle(predecessors);
           // Return the negative cycle and its length.
           return new BFYResult.NegativeCycle(cycle, calculateCycleLength(graph, cycle));
         } else {
@@ -73,12 +82,12 @@ public BFYResult compute(WeightedDigraph graph, int from) {
       // ...then relax the edges outgoing from the vertex.
       for (WeightedDigraph.Edge edge : graph.getOutgoingEdges(current)) {
         int neighbor = edge.to();
-        int newDist = distance[current] + edge.weight();
-        // If the new distance is shorter...
-        if (distance[neighbor] > newDist) {
-          // ...then update the distance and predecessor.
-          distance[neighbor] = newDist;
-          predecessor[neighbor] = current;
+        int newDist = distances[current] + edge.weight();
+        // If the new distances is shorter...
+        if (distances[neighbor] > newDist) {
+          // ...then update the distances and predecessors.
+          distances[neighbor] = newDist;
+          predecessors[neighbor] = current;
           // If the neighbor is not in the queue...
           if (!inQueue[neighbor]) {
             // ...then add it to the queue and mark it as in the queue.
@@ -91,35 +100,35 @@ public BFYResult compute(WeightedDigraph graph, int from) {
   }
 
   // Return the shortest path tree.
-  return new BFYResult.ShortestPathTree(distance, predecessor);
+  return new BFYResult.ShortestPathTree(distances, predecessors);
 }
 
  /**
- * This method finds the first negative cycle in a graph using the Bellman-Ford algorithm.
+ * This method finds a negative cycle in a graph using the Bellman-Ford algorithm.
  * It uses the predecessor array that was computed by the Bellman-Ford algorithm.
  *
- * @param predecessor The array of predecessors for each vertex on the shortest path from the source.
+ * @param predecessors The array of predecessors for each vertex on the shortest path from the source.
  * @return A list of vertices that form a negative cycle, or an empty list if no such cycle exists.
  */
-private List<Integer> findNegativeCycle(int[] predecessor) {
+private List<Integer> findNegativeCycle(int[] predecessors) {
   // This array keeps track of the vertices that have been visited.
-  boolean[] visited = new boolean[predecessor.length];
+  boolean[] visited = new boolean[predecessors.length];
   // This list will hold the vertices of the negative cycle, if one is found.
   List<Integer> cycle = new LinkedList<>();
 
   // Iterate over all vertices.
-  for (int i = 0; i < predecessor.length; i++) {
+  for (int i = 0; i < predecessors.length; i++) {
     // If the vertex has been visited, skip it.
     if (visited[i]) continue;
 
     // This array keeps track of the vertices visited in the current cycle.
-    boolean[] cycleCheck = new boolean[predecessor.length];
+    boolean[] cycleCheck = new boolean[predecessors.length];
     int current = i;
 
     // Traverse the predecessors until we reach an unreachable vertex or a vertex that has been visited in the current cycle.
     while (current != BFYResult.UNREACHABLE && !cycleCheck[current]) {
       cycleCheck[current] = true;
-      current = predecessor[current];
+      current = predecessors[current];
     }
 
     // If we found a cycle...
@@ -127,12 +136,12 @@ private List<Integer> findNegativeCycle(int[] predecessor) {
       // ...then add the vertices of the cycle to the cycle list.
       int start = current;
       cycle.add(start);
-      current = predecessor[start];
+      current = predecessors[start];
       while (current != start) {
         cycle.add(current);
-        current = predecessor[current];
+        current = predecessors[current];
       }
-      cycle.add(start); // To complete the cycle
+      cycle.add(start); // To complete the cycle by adding the starting vertex again.
       break;
     }
 
@@ -154,7 +163,7 @@ private List<Integer> findNegativeCycle(int[] predecessor) {
  *
  * @param graph The weighted directed graph that contains the cycle.
  * @param cycle A list of vertices that form a cycle in the graph.
- * @return The total weight of the cycle.
+ * @return The total weight of the cycle .
  */
 private int calculateCycleLength(WeightedDigraph graph, List<Integer> cycle) {
   // Initialize the length of the cycle to 0.
--- src/main/java/gre/lab2/groupG/Main.java
+++ src/main/java/gre/lab2/groupG/Main.java
@@ -5,10 +5,22 @@ import java.io.IOException;
 import gre.lab2.graph.BFYResult;
 import gre.lab2.graph.WeightedDigraph;
 import gre.lab2.graph.WeightedDigraphReader;
+import java.io.File;
 
+/**
+ * This is the main class of the application. It contains methods to process a directory of files,
+ * where each file contains data for a weighted directed graph. The Bellman-Ford-Yens algorithm is
+ * applied to each graph, and the results are printed.
+ *
+ * @author Cristhian Ronquillo
+ */
 public final class Main {
+
+  // The source vertex from which to compute the shortest path tree.
+  private static final int SOURCE = 0;
+
   public static void main(String[] args) throws IOException {
-    // TODO
+    // TODO:
     //  - Renommage du package ;
     //  - Écrire le code dans le package de votre groupe et UNIQUEMENT celui-ci ;
     //  - Documentation soignée comprenant :
@@ -21,32 +33,79 @@ public final class Main {
     // BFYResult result = bfy.compute(graph, 0);
     // System.out.println(result);
 
-    WeightedDigraph graph51 = WeightedDigraphReader.fromFile("data/reseau5_1.txt");
-    BellmanFordYensAlgorithm bfy1 = new BellmanFordYensAlgorithm();
-    BFYResult result51 = bfy1.compute(graph51, 0);
-    System.out.println(result51);
-    //(0,—) (1,3) (5,1) (4,0)
+    // Specify the path of the directory to read
+    String directoryPath = "data";
 
-    //***********************************************************************
-    WeightedDigraph graph52 = WeightedDigraphReader.fromFile("data/reseau5_2.txt");
-    BellmanFordYensAlgorithm bfy2 = new BellmanFordYensAlgorithm();
-    BFYResult result52 = bfy2.compute(graph52, 0);
-    System.out.println(result52);
-    //(0,—) (0,3) (4,1) (1,0) (3,2) (∞,—)
+    // Create a File object for the directory
+    File directory = new File(directoryPath);
 
-    //***********************************************************************
-    WeightedDigraph graph53 = WeightedDigraphReader.fromFile("data/reseau5_3.txt");
-    BellmanFordYensAlgorithm bfy3 = new BellmanFordYensAlgorithm();
-    BFYResult result53 = bfy3.compute(graph53, 0);
-    System.out.println(result53);
-    //  Le réseau contient donc un circuit à coût négatif accessible depuis le sommet 1.
+    // Process the directory
+    processDirectory(directory);
+  }
 
-    //***********************************************************************
-    WeightedDigraph graph4 = WeightedDigraphReader.fromFile("data/reseau4.txt");
-    BellmanFordYensAlgorithm bfy4 = new BellmanFordYensAlgorithm();
-    BFYResult result4 = bfy4.compute(graph4, 0);
-    System.out.println(result4);
-    //  Le réseau contient donc un circuit à coût négatif accessible depuis le sommet 1.
+  /**
+   * Processes all the files in the specified directory.
+   *
+   * @param directory the directory to process
+   * @throws IOException if an I/O error occurs
+   */
+  private static void processDirectory(File directory) throws IOException {
+    // Check if the path is a directory
+    if (directory.isDirectory()) {
+      // List the files in the directory
+      File[] files = directory.listFiles();
 
+      // Iterate over the array of files
+      if (files != null) {
+        for (File file : files) {
+          // Check if the path is a file
+          if (file.isFile()) {
+            // Process the file
+            processFile(file);
+          }
+        }
+      } else {
+        System.out.println("The directory is empty.");
+      }
+    } else {
+      System.out.println("The specified path is not a directory.");
+    }
+  }
+
+  /**
+   * Processes a single file by reading the graph data, applying the algorithm of Bellman Ford Yens,
+   * and printing the results.
+   *
+   * @param file the file to process, this file contains the graph data
+   * @throws IOException if an I/O error occurs
+   */
+  private static void processFile(File file) throws IOException {
+    // Read the graph from the provided file
+    WeightedDigraph graph = WeightedDigraphReader.fromFile(file.getAbsolutePath());
+
+    // Create an instance of the algorithm and execute the computation
+    BellmanFordYensAlgorithm algorithm = new BellmanFordYensAlgorithm();
+    BFYResult result = algorithm.compute(graph, SOURCE);
+
+    // Print the file name
+    System.out.println();
+    System.out.println("---------------------------------------------");
+    System.out.println("File: " + file.getName());
+
+    // Display the results based on the type of result returned
+    if (result.isNegativeCycle()) {
+      BFYResult.NegativeCycle negativeCycle = result.getNegativeCycle();
+      System.out.println("A negative cycle has been detected.");
+      System.out.println("The length (total weight) of the negative cycle is: " + negativeCycle.length());
+      System.out.println("The vertices of the negative cycle are: " + negativeCycle.vertices());
+    } else {
+      BFYResult.ShortestPathTree spt = result.getShortestPathTree();
+      System.out.println("Shortest path tree found.");
+      if (graph.getNVertices() < 25) {
+        System.out.println(spt);
+      } else {
+        System.out.println("The graph has more than 25 vertices, the results are not displayed.");
+      }
+    }
   }
 }
